deb4252a616289452d810ea547a421cae84e4d0b
Ruben Alcazar <alkazar.r@gmail.com>
2017-07-10 16:15:21 -0500

    fix adding members search

---
+++ src/Screens/MembersScreen/MembersAdder/styles.js
@@ -0,0 +1,33 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  modal: {
+  },
+  container: {
+    justifyContent: 'center',
+    flex: 1,
+    backgroundColor: 'rgba(52, 52, 52, 0.8)'
+  },
+  content: {
+    paddingTop: 20,
+    height: 200,
+    backgroundColor: 'white',
+  },
+  button: {
+    backgroundColor: '#C7584E',
+    padding: 10,
+    margin: 10,
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+  buttonText: {
+    fontSize: 14,
+    color: 'white',
+  },
+  textInput: {
+    padding: 10,
+    width: '100%',
+    height: 50,
+    backgroundColor: 'white',
+  },
+});
--- src/Screens/MembersScreen/index.js
+++ src/Screens/MembersScreen/index.js
@@ -3,20 +3,80 @@ import {
   View,
   Text,
   TouchableOpacity,
+  FlatList,
 } from 'react-native';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-import { Header, Left, Center, Right } from '../../Components';
 import styles from './styles';
+import MembersAdder from './MembersAdder';
+import { Header, Left, Center, Right } from '../../Components';
 
-export default class MembersScreen extends Component {
+class MembersScreen extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showAdder: false,
+      membersSearch: '',
+    };
+  }
 
   handleBackPress = () => {
     this.props.navigation.navigate('Home');
   }
 
-  render() {
+  renderMember = ({ item }) => {
     return (
       <View>
+        <Text>{item.username}{item.owner ? '(owner)' : ''}</Text>
+      </View>
+    );
+  }
+
+  handleShowAdderPress = () => {
+    this.setState({ showAdder: true, membersSearch: '' });
+  }
+
+  handleCloseAdderPress = () => {
+    this.setState({ showAdder: false });
+  }
+
+  updateMembersSearch = (text) => {
+    this.setState({ membersSearch: text });
+  }
+
+  refetch = () => {
+    const query = this.props.todoList;
+    query.refetch(query.variables);
+  }
+
+  render() {
+    console.log('props', this.props);
+    const { getTodoList, loading } = this.props.todoList;
+
+    if (loading) {
+      return (
+        <View><Text>Loading...</Text></View>
+      )
+    }
+
+    const members = getTodoList.members.edges.map((edge) => {
+      return edge.node;
+    });
+    const owner = getTodoList.createdBy;
+    members.unshift({
+      owner: true,
+      ... owner,
+    });
+
+    console.log('members', members);
+
+    return (
+      <View>
+
+        {/** Header **/}
         <Header>
           <Left>
             <TouchableOpacity onPress={this.handleBackPress}>
@@ -27,19 +87,64 @@ export default class MembersScreen extends Component {
             <Text>Members</Text>
           </Center>
           <Right>
-            <TouchableOpacity onPress={this.handleBackPress}>
+            <TouchableOpacity onPress={this.handleShowAdderPress}>
               <Icon name="add" size={30} />
             </TouchableOpacity>
           </Right>
         </Header>
 
+        {/** Members **/}
         <View>
-          <View>
-
-          </View>
+          <FlatList
+            data={members}
+            renderItem={this.renderMember}
+            keyExtractor={(item) => item.id}
+            ListEmptyComponent={<View><Text>Empty</Text></View>}
+          />
         </View>
+
+        {/** Members Adder **/}
+        <MembersAdder
+          visible={this.state.showAdder}
+          close={this.handleCloseAdderPress}
+          todoList={getTodoList}
+          membersSearch={this.state.membersSearch}
+          updateMembersSearch={this.updateMembersSearch}
+          refetch={this.refetch}
+        />
 
       </View>
     );
   }
 }
+
+const getTodoListQuery = gql`
+  query GetTodoList($id: ID!) {
+    getTodoList(id: $id) {
+      id
+      createdAt
+      createdBy {
+        id
+        username
+      }
+      members {
+        edges {
+          node {
+            id
+            username
+          }
+        }
+      }
+    }
+  }
+`;
+
+export default graphql(getTodoListQuery, {
+  name: 'todoList',
+  options: props => ({
+    variables: {
+      id: props.navigation.state.params.node.id,
+    },
+    pollInterval: 5000, // Time for development
+  }),
+})(MembersScreen);
